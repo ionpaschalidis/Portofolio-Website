98d00edeaf06e3ef9c800cec46965ccb9ce936ca
ion <ionpaschalidis@gmail.com>
2025-04-06 15:25:48 +0200

    Dark and Light mode added

---
+++ modeSwitch.js
@@ -0,0 +1,19 @@
+let lightmode = localStorage.getItem('lightmode');
+const themeSwitch = document.getElementById('theme-switch'); // button
+
+const enableLightMode = () => {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'active');
+};
+
+const disableLightMode = () => {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode', null);
+};
+
+if (lightmode === "active") enableLightMode()
+
+themeSwitch.addEventListener("click", () => {
+    lightmode = localStorage.getItem('lightmode'); // update value
+    lightmode !== "active" ? enableLightMode() : disableLightMode();
+});
